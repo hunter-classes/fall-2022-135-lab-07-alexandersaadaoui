222a6a6df9c285bc274791a371477ae191e96520
Mac Koltovskiy <mackolt@DESKTOP-P2PHME8.localdomain>
2022-12-12 11:39:40 -0500

    comments

--- functions.cpp
+++ functions.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cctype>
 
+//remove starting spaces
 std::string removeLeadingSpaces(std::string line){
     std::string result;
     for(int i = 0; i < line.length(); i++){
@@ -14,6 +15,7 @@ std::string removeLeadingSpaces(std::string line){
     return result;
 }
 
+//count how many times a certain character appears
 int countChar(std::string line, char c){
     int result = 0;
     for(int i = 0; i < line.length(); i++){
@@ -24,6 +26,7 @@ int countChar(std::string line, char c){
     return result;
 }
 
+//indents the line 
 std::string indenting(std::string line, int num){
     std::string indents;
     for (int i = 0; i < num; i++){
